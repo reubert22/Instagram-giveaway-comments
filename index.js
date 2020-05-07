f8769fedc3fc7454d5bd0c1da7081ee5b9b6d401
reubert22 <reubertfernandes@gmail.com>
2020-05-07 19:03:01 -0300

    Adding looping to comment 10 times on giveaway, then redirect

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const targetPageUrl = 'https://www.instagram.com/';
-  const targetsAccount = [];
+  const targetsAccount = [
+    '@example_name'
+  ];
   const giveawaysTarget = [
     {
-      profile: 'https://www.instagram.com/wesley_alemao_/', 
-      giveaway: 'https://www.instagram.com/p/B_YMGKpnhrZ/', 
+      profile: 'https://www.instagram.com/motoart22/', 
+      giveaway: 'https://www.instagram.com/p/B-QGm4lFfYa/',
       date: '08/05', 
-      profileName: 'wesley_alemao_'
+      profileName: 'motoart22'
+    },
+    {
+      profile: 'https://www.instagram.com/hugo.milgrau/', 
+      giveaway: 'https://www.instagram.com/p/B_pkTiWFKvh/', 
+      date: '10/05', 
+      profileName: 'hugo.milgrau'
+    },  
+    {
+      profile: 'https://www.instagram.com/arnondograu_/', 
+      giveaway: 'https://www.instagram.com/p/B_p16xxlahi/', 
+      date: '20/05', 
+      profileName: 'arnondograu_'  
+    },  
+    {
+      profile: 'https://www.instagram.com/teteurr46/', 
+      giveaway: 'https://www.instagram.com/p/B_pkMbbAtrt/', 
+      date: '20/05', 
+      profileName: 'teteurr46'
     }
   ];
 
@@ -17,6 +37,12 @@ const puppeteer = require('puppeteer');
     const giveaway = giveawaysTarget[number];
     console.log('** Redirecting to: ' + giveaway.profileName + ' giveaway, date: ' + giveaway.date + ' **');
     return giveaway;
+  }
+
+  const handleProfileToComment = () => {
+    const number = Math.floor(Math.random() * targetsAccount.length);
+    const profile = targetsAccount[number];
+    return profile;
   }
 
   const browser = await puppeteer.launch({ headless: false });
@@ -34,9 +60,26 @@ const puppeteer = require('puppeteer');
   await page.waitFor(5000);
   await page.goto(giveawayToGo.giveaway);
 
-  await page.waitFor(10000);
-  await page.waitFor('textarea');
-  await page.type('textarea', '@profile_name', { delay: 642 });
+  count = 0;
+  while(true) {
+    if (count === 10) {
+      await page.waitFor(15000);
+      const giveawayToGo = handleRedirect();
+      await page.goto(giveawayToGo.profile);
+      await page.waitFor(6000);
+      await page.goto(giveawayToGo.giveaway);
+      count = 0;
+    }
+    await page.waitFor(10402);
+    const profile = handleProfileToComment();
+    
+    await page.waitFor('textarea');
+    await page.type('textarea', profile, {delay: 790});
+    await page.click('button[type="submit"]');
+    count += 1;
+    console.log('contador: ', count, ' > ', profile)
+    await page.waitFor(5000);
+  };
 
   await browser.close();
 })();
